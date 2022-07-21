97796a283aa0d136d75cf4e2f74b694ad133ab00
Eduard <30580773+bar-alex@users.noreply.github.com>
2022-07-21 22:54:06 +0100

    Update model.js
    Changed back as per Michael's request, by removing fullName, email, bio, and renamed password back to hashedPassword in the model, removed the logic and bulkCreate hook
    
    The hooks expect that the user document has a 'password' field with the password in the clear which will get removed after hashing and storing the hashed value into hashedPassword
    
    validatePassword() will compare the password received as a parameter to the hashed value in the hashedPassword field.

--- src/user/model.js
+++ src/user/model.js
@@ -4,10 +4,7 @@
 
 // id       - integer (auto create by MySql/Postgres), PK
 // username - String, unique, required
-// password - String, unique, required // my proposal, because we know passwords are actually hashed, and we'll also abstract it as well
-// fullName - String, required
-// email    - String, required, unique
-// bio      - Text
+// hashedPassword - String, unique, required // my proposal, because we know passwords are actually hashed, and we'll also abstract it as well
 // hasRopes - Boolean, required
 // location - String, required
 
@@ -23,22 +20,10 @@ const User = connection.define( "User", {
             unique: true,
             allowNull: false,
         },
-        password: {
+        hashedPassword: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true,
-        },
-        fullName: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        bio: {
-            type: DataTypes.TEXT,
         },
         hasRopes: {
             type: DataTypes.BOOLEAN,
@@ -59,19 +44,8 @@ const hashPassword = async(user, options) => {
     const salt = await bcrypt.genSalt(parseInt(saltRounds));
     const hash = await bcrypt.hash(user.password, salt);
     //console.log('-> User.beforeCreate: user = ', user);
-    user.password = hash;
-}
-
-// hash the passwords of all the users n the list provided
-const hashPasswordBulk = async (userList) => {
-    const saltRounds = process.env.SALT_ROUNDS;
-    const salt = await bcrypt.genSalt(parseInt(saltRounds));
-    
-    for (user of userList){
-        const hash = await bcrypt.hash(user.password, salt);
-        //console.log('-> User.beforeCreate: user = ', user);
-        user.password = hash;
-    }
+    delete user.password;       // the unhashed password (that was received from the FE) gets removed
+    user.hashedPassword = hash;
 }
 
 
@@ -81,13 +55,9 @@ User.addHook('beforeUpdate', hashPassword);
 User.addHook('beforeSave', hashPassword);
 
 
-// for bulk create -- if we're ever gonna use it
-User.addHook('beforeBulkCreate', hashPasswordBulk)
-
-
 // validate that the provided password is correct
 User.prototype.validatePassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.hashedPassword);
 }
 
 
